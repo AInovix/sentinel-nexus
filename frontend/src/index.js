d72ff3874e157893b2c2023a13d7d79c07bba3aa
AInovix <AInovix@proton.me>
2026-01-17 23:35:22 +1000

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-import 'leaflet/dist/leaflet.css';
-import './index.css';
+import './index.css';  // if you have global styles
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,5 +9,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-reportWebVitals();
